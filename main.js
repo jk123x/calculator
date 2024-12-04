4e44c21d1fca3fc2ced045768b4efd8a4d817bb0
jk123x <jay.kinkade@gmail.com>
2024-12-05 03:35:44 +1100

    pen to paper

---
+++ main.js
@@ -0,0 +1,29 @@
+const add = function(a, b) {
+    return a + b;
+    };
+
+const subtract = function(a, b) {
+    return a - b;
+    };
+
+const multiply = function (a, b) {
+    return a * b;
+    };
+
+const divide = function(a, b) {
+    return a / b;
+};
+
+const operators = {
+    '+': function(a, b) { return a + b},
+    '-': function(a, b) { return a - b},
+    '*': function(a, b) { return a * b},
+    '%': function(a, b) { return a / b},
+};
+
+const a = a;
+const b = b;
+
+// function operate {
+    
+// };
