01683fe7db79622f42ff5f2736efc7c5a0031f51
jk123x <jay.kinkade@gmail.com>
2024-12-26 04:27:48 +1100

    code works but can't see output

--- main.js
+++ main.js
@@ -1,42 +1,164 @@
-const add = function(a, b) {
-    return a + b;
-    };
+let currentNum = "";
+let previousNum = "";
+let operator = "";
 
-const subtract = function(a, b) {
-    return a - b;
-    };
+const currentDisplayNumber = document.querySelector(".currentNumber");
+const previousDisplayNumber = document.querySelector(".previousNumber");
 
-const multiply = function (a, b) {
-    return a * b;
-    };
+window.addEventListener("keydown", handleKeyPress);
 
-const divide = function(a, b) {
-    return a / b;
-};
+const operators = document.querySelectorAll(".operator");
+const numberButton = document.querySelectorAll(".numberButton");
 
-const operators = {
-    '+': function(a, b) { return a + b},
-    '-': function(a, b) { return a - b},
-    '*': function(a, b) { return a * b},
-    '%': function(a, b) { return a / b},
-};
+const clear = document.querySelector(".clear");
+clear.addEventListener("click", clearCalculator);
 
-// let a = a;
-// let b = b;
+const equal = document.querySelector(".equal");
+equal.addEventListener("click", () => {
+    if (currentNum != "" && previousNum != "") {
+        compute();
+    }
+});
 
-const numberButtons = document.querySelectorAll(".numberButton");
-const display = document.querySelector(".display");
+const decimal = document.querySelector(".decimal");
+decimal.addEventListener("click", () => {
+    addDecimal();
+});
 
-numberButtons.forEach(function(numberButton) {
-    numberButton.addEventListener("click", function() {
-        display.textContent += numberButton.value;
-        currentValue = parseInt(display.innerText);
+numberButton.forEach((number) => {
+    number.addEventListener("click", (e) => {
+        handleNumber(e.target.textContent);
     })
-})
+});
+
+function handleNumber(numberButton) {
+    if (previousNum !== "" && currentNum !== "" && operator === "") {
+        previousNum = "";
+        currentDisplayNumber.textContent = currentNum;
+    }
+    if (currentNum.length <= 10) {
+      currentNum += numberButton;
+      currentDisplayNumber.textContent = currentNum;
+    }
+}; 
+
+    operators.forEach((operators) => {
+        operators.addEventListener("click", (e) => {
+            handleOperator(e.target.textContent);
+        });
+    });
+
+function handleOperator(op) {
+    if (previousNum === "") {
+        previousNum = currentNum;
+        operatorCheck(op);
+        } else if (currentNum === "") {
+        operatorCheck(op);
+        } else {
+            compute();
+            operator = op;
+            currentDisplayNumber.textContent = "0";
+            previousDisplayNumber.textContent = previousNum + " " + operator;
+        }
+    };
+
+    function operatorCheck(text) {
+        operator = text;
+        previousDisplayNumber.textContent = previousNum + " " + operator;
+        currentDisplayNumber.textContent = "0";
+        currentNum = "";
+    }
+   
+
+    function compute() {
+        previousNum = Number(previousNum);
+        currentNum = Number(currentNum);
+
+        if (operator === '+') {
+            previousNum += currentNum;
+        } else if (operator === '-') {
+            previousNum -= currentNum;
+        } else if (operator === '*') {
+            previousNum *= currentNum;
+        } else if (operator === "/") {
+            if (currentNum <= 0) {
+                previousNum = "C'mon, Don't Do That!";
+                displayResults();
+                return;
+            }
+            previousNum /= currentNum;
+        }
+        previousNum = roundNumber(previousNum);
+        previousNum = previousNum.toString();
+        displayResults();
+        };
     
 
+        function roundNumber(num) {
+            return Math.round(num * 100000) / 100000;
+        }
+  
+        function displayResults() {
+            if (previousNum.length <= 10) {
+                currentDisplayNumber.textContent = previousNum;
+            } else {
+                currentDisplayNumber.textContent = previousNum.slice(0, 10) + "...";
+            }
+            previousDisplayNumber.textContent = "";
+            operator = "";
+            currentNum = "";
+            }
 
+            function clearCalculator() {
+                currentNum = "";
+                previousNum = "";
+                operator = "";
+                currentDisplayNumber.textContent = "0";
+                previousDisplayNumber.textContent = "";
+            }
 
-// function operate {
-    
-// };
+            function addDecimal() {
+                if (!currentNum.includes(".")) {
+                    currentNum += ".";
+                    currentDisplayNumber.textContent = currentNum;
+                }
+            }
+
+            function handleKeyPress(e) {
+                e.preventDefault();
+                if (e.key >= 0 && e.key <= 9) {
+                    handleNumber(e.key);
+                }
+                if (
+                    e.key === "Enter" ||
+                    (e.key === "=" && currentNum != "" && previousNum != "")
+                ) {
+                    compute();
+                }
+                if (e.key === "+" || e.key === "-" || e.key === "/") {
+                    handleOperator(e.key);
+                } 
+                if (e.key === "*") {
+                    handleOperator("x");
+                }
+                if (e.key === ".") {
+                    addDecimal();
+                }
+                if (e.key === "Backspace") {
+                    handleDelete();
+                  }
+                }
+            
+                function handleDelete() {
+                    if (currentNum !== "") {
+                        currentNum = currentNum.slice(0, -1);
+                        currentDisplayNumber.textContent = currentNum;
+                        if (currentNum === "") {
+                            currentDisplayNumber.textContent = "0";
+                        }
+                    }
+                    if (currentNum === "" && previousNum !== "" && operator === "") {
+                        previousNum = previousNum.slice(0, -1);
+                        currentDisplayNumber.textContent = previousNum;
+                    }
+                }
